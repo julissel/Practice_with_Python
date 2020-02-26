659e2baf30e4532b6e044de3fdc4c9381b334f38
Julia Selyutina <jusereda@gmail.com>
2020-02-26 19:09:54 +0300

    The program in file "check_squared_elements.py" checks that the elements in "b" are the elements in "a" squared, regardless of the order.

---
+++ venv/include/check_squared_elements.py
@@ -0,0 +1,14 @@
+'''
+Given two arrays a and b write a function comp(a, b)
+that checks whether the two arrays have the "same" elements, with the same multiplicities.
+"Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+Example:
+Valid arrays:
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+'''
+def comp(array1, array2):
+    try:
+        return sorted([i * i for i in array1]) == sorted(array2)
+    except:
+        return False
